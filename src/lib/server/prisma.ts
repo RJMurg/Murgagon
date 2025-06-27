fd4a3e4b79201bfd8fd67ecbbb90208aada518f4
Ruán <ruan@murgatroyd.me>
2025-06-27 03:16:41 +0100

    Hacky Wacky Fix

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,2 +1,8 @@
-import { PrismaClient } from '@prisma-app/client';
-export const prisma = new PrismaClient();
+import type { PrismaClient as PrismaClientType } from '@prisma/client';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url ?? __filename);
+
+const { PrismaClient: PrismaClientImpl } = require('@prisma/client');
+
+export class PrismaClient extends (PrismaClientImpl as typeof PrismaClientType) {}
