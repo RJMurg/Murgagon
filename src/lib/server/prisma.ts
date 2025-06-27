488ba6d839540650bb988f1a545e45882b628fe6
Ruán <ruan@murgatroyd.me>
2025-06-27 03:30:23 +0100

    Something tells me that Upgrading to Prisma 7 is going to absolutely kill me

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,8 +1,2 @@
-import type { PrismaClient as PrismaClientType } from '@prisma/client';
-import { createRequire } from 'module';
-
-const require = createRequire(import.meta.url ?? __filename);
-
-const { PrismaClient: PrismaClientImpl } = require('@prisma-app/client');
-
-export class prisma extends (PrismaClientImpl as typeof PrismaClientType) {}
+import { PrismaClient } from '@prisma/client';
+export const prisma = new PrismaClient();
